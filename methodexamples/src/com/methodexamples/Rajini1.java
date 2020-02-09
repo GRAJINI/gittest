2a0b9d0ba00696dae053d5c3cbb57babe68a1154
Admin <Admin@Admin-PC>
2020-02-09 22:07:06 +0530

    send

---
+++ methodexamples/src/com/methodexamples/Rajini1.java
@@ -0,0 +1,16 @@
+package com.methodexamples;
+
+public class Rajini1 
+{
+	 int a=10;
+	public void method()
+	{
+	System.out.println(a);
+	}
+	public static void main(String[]args)
+	{
+		Rajini1 r=new Rajini1();
+		
+	System.out.println(r.a);
+	}
+}
