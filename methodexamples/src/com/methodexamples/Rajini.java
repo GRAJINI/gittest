f4867cc256fd747c489deb30b591545e407f192e
Admin <Admin@Admin-PC>
2020-02-09 22:09:15 +0530

    send
    Change-Id: I8cd78460187950088b23f3d83f108fb2d6349684

---
+++ methodexamples/src/com/methodexamples/Rajini.java
@@ -0,0 +1,14 @@
+package com.methodexamples;
+
+public class Rajini
+{
+    int i=20; //non static variable are not allowed in static method incase we want to use we can declare static keyword otherwise declare the variable inside static method
+    
+	   public static void main(String[] args)
+	   {
+		   int i=40;
+	       System.out.println(i);	
+
+	    }
+
+}
